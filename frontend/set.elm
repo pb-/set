3085a805e0740947432fa666084c048a77da5d65
Paul Baecher <pbaecher@gmail.com>
2018-09-23 19:10:38 +0200

    First render

--- frontend/set.elm
+++ frontend/set.elm
@@ -1,10 +1,19 @@
-import Html exposing (Html, button, div, text, input)
+import Html exposing (Html, button, div, text, input, tr, td, table)
 import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (placeholder)
+import Html.Attributes exposing (placeholder, style, class)
+import Svg exposing (path, svg)
+import Svg.Attributes exposing (viewBox, d)
 import WebSocket exposing (listen, send)
 import Json.Decode exposing (Decoder, field, map2, int, string, list, bool, maybe, decodeString)
 
-serverAddress = "ws://localhost:8001"
+serverAddress = "ws://10.0.0.8:8001"
+
+shapes i =
+  case i of
+    0 -> "M50 10 l40 20 l-40 20 l-40 -20 Z"
+    1 -> "M25 45 h50 a15 15 0 0 0 0 -30 h-50 a15 15 0 0 0 0 30"
+    2 -> "M20 30 m-9.19 -9.19 a13 13 0 0 0 18.38 18.38 a8.21 8.21 0 0 1 11.62 0 a34.21 34.21 0 0 0 48.38 0 a13 13 0 0 0 -18.38 -18.38 a8.21 8.21 0 0 1 -11.62 0 a34.21 34.21 0 0 0 -48.38 0"
+    _ -> ""
 
 main =
   Html.program
@@ -38,6 +47,13 @@ type alias Player =
 type alias Game =
   { board : Board
   , gameOver: Bool
+  }
+
+type alias Card =
+  { color : Int
+  , shape : Int
+  , count : Int
+  , shading : Int
   }
 
 type alias Board = List (List Int)
@@ -107,11 +123,22 @@ subscriptions model =
 
 -- VIEW
 
+decodeCard : Int -> Card
+decodeCard card =
+  { color = card % 3
+  , shape = card // 3 % 3
+  , count = card // 9 % 3
+  , shading = card // 27 % 3
+  }
+
 
 view : Model -> Html Msg
 view model =
   if model.joined then
-    div [] [ text (toString model) ]
+    div []
+      [ text (toString model)
+      , viewState model
+      ]
   else
     viewLogin model
 
@@ -121,3 +148,41 @@ viewLogin model =
     [ input [ placeholder "Your name", onInput UpdateName ] []
     , button [ onClick Join ] [ text "join" ]
     ]
+
+viewState : Model -> Html Msg
+viewState model =
+  div []
+    [ viewGame model
+    , viewPlayers model
+    ]
+
+viewGame : Model -> Html Msg
+viewGame model =
+  case model.server.game of
+    Nothing ->
+      div [] [text "Waiting for another player"]
+    Just game ->
+      div
+        [ style [("grid-template-columns", "repeat(" ++ (toString (List.length game.board)) ++ ", 1fr)")]
+        , class "board"
+        ]
+        (List.map viewCard (List.concat game.board))
+
+viewCard : Int -> Html Msg
+viewCard card =
+  let
+    c = decodeCard card
+  in
+  div [class ("card col" ++ (toString c.color) ++ " sh" ++ (toString c.shading))]
+    (List.repeat (c.count + 1) (svg [viewBox "-10 5 120 50"] [path [d (shapes c.shape)] []]))
+
+viewPlayers : Model -> Html Msg
+viewPlayers model =
+  let
+    viewPlayer player =
+      tr []
+        [ td [] [ text player.name ]
+        , td [] [ text (toString player.points) ]
+        ]
+  in
+    table [] (List.map viewPlayer model.server.players)
